3a9fb7311974a3aa062757ba8900d666d7432629
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-30 01:20:07 +0530

    gender table added and updated

--- PayRollServices.sql
+++ PayRollServices.sql
@@ -11,7 +11,7 @@ CREATE TABLE employee_payroll
 	StartDate date
 	)
 
---UC-3-Create-Employee-Payroll-Data-As-Part-Of-CURD-Operation--
+--UC3-Create-Employee-Payroll-Data-As-Part-Of-CURD-Operation--
 INSERT INTO employee_payroll (EmpName, Salary, StartDate) VALUES
 	('Hitesh', 40000.00, '2005-12-05'),
 	('Dheeraj', 50000.00, '2008-03-02'),
@@ -19,9 +19,13 @@ INSERT INTO employee_payroll (EmpName, Salary, StartDate) VALUES
 	('Umang', 40000.00, '2016-01-03'),
 	('Rahul', 50000.00, '2018-06-10')
 
---UC-4-Retrieve-Employee-Payroll-Data--
+--UC4-Retrieve-Employee-Payroll-Data--
 SELECT * FROM employee_payroll;
 
---UC-5-Retrieve-Salary-Of-Particular-Employee-And-Particular-Date-Range--
+--UC5-Retrieve-Salary-Of-Particular-Employee-And-Particular-Date-Range--
 SELECT salary FROM employee_payroll WHERE EmpName = 'Vishnu'
 SELECT * FROM employee_payroll WHERE StartDate between cast ('2015-07-01' as date) and GETDATE();
+
+--UC6-Add-Gender-To-Employee-Payroll-Table-And-Update--
+ALTER TABLE employee_payroll ADD Gender char(1)
+update employee_payroll set Gender = 'M' where Id in (1,2,3,4,5) 
