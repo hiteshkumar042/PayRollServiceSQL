e7725a01c7cf71d651107ef9670c3599b941a9b6
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-30 00:50:36 +0530

    Employee table created

--- PayRollServices.sql
+++ PayRollServices.sql
@@ -1,3 +1,12 @@
---UC-1-Create-Payroll-Service-Database--
+--UC1-Create-Payroll-Service-Database--
 CREATE DATABASE Payroll_Service
 USE Payroll_Service
+
+--UC2-Add Employee Table--
+Create Table employee_payroll
+	(
+	Id int identity (1,1) primary key,
+	EmpName varchar(200),
+	Salary float,
+	StartDate date
+	)
