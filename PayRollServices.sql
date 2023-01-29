bb707399d05a44e17f179f751e5cd600223ae841
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-30 01:01:48 +0530

    Retrieved all Data

--- PayRollServices.sql
+++ PayRollServices.sql
@@ -3,7 +3,7 @@ CREATE DATABASE Payroll_Service
 USE Payroll_Service
 
 --UC2-Add Employee Table--
-Create Table employee_payroll
+CREATE TABLE employee_payroll
 	(
 	Id int identity (1,1) primary key,
 	EmpName varchar(200),
@@ -12,9 +12,12 @@ Create Table employee_payroll
 	)
 
 --UC-3-Create-Employee-Payroll-Data-As-Part-Of-CURD-Operation--
-insert into employee_payroll (EmpName, Salary, StartDate) values
+INSERT INTO employee_payroll (EmpName, Salary, StartDate) VALUES
 	('Hitesh', 40000.00, '2005-12-05'),
 	('Dheeraj', 50000.00, '2008-03-02'),
 	('Vishnu', 60000.00, '2007-04-07'),
 	('Umang', 40000.00, '2016-01-03'),
 	('Rahul', 50000.00, '2018-06-10')
+
+--UC-4-Retrieve-Employee-Payroll-Data--
+SELECT * FROM employee_payroll;
