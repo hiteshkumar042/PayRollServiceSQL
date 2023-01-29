1fafdd96e88ae245bfe18631638c23683889643e
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-30 00:58:11 +0530

    Data inserted in table

--- PayRollServices.sql
+++ PayRollServices.sql
@@ -10,3 +10,11 @@ Create Table employee_payroll
 	Salary float,
 	StartDate date
 	)
+
+--UC-3-Create-Employee-Payroll-Data-As-Part-Of-CURD-Operation--
+insert into employee_payroll (EmpName, Salary, StartDate) values
+	('Hitesh', 40000.00, '2005-12-05'),
+	('Dheeraj', 50000.00, '2008-03-02'),
+	('Vishnu', 60000.00, '2007-04-07'),
+	('Umang', 40000.00, '2016-01-03'),
+	('Rahul', 50000.00, '2018-06-10')
