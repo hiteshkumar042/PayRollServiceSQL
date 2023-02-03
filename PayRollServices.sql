d01d258af199acd2f3afdf6827b40e79133a129d
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-02-03 20:06:15 +0530

    UC9-salary Added

--- PayRollServices.sql
+++ PayRollServices.sql
@@ -42,3 +42,11 @@ select * from employee_payroll
 alter table employee_payroll add Phone bigint;
 ALTER TABLE employee_payroll add Department varchar(200) not null default 'IT';
 ALTER TABLE employee_payroll add Address varchar(200) default 'Bangalore';
+
+--UC-9-Extend Table To Have Basic Pay, Deductions, Taxable Pay, Income Tax, Net Pay--
+--exec sp_rename 'employee_payroll.salary','Basic_pay','column';
+alter table employee_payroll add 
+								 Deductions float not null default 0.00,
+								 Taxable_Pay float not null default 0.00, 
+								 Income_Tax float not null default 0.00,
+								 Net_Pay float not null default 0.00;
